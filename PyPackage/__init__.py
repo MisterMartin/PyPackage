a51a22da577a66670ca39a836cf59986ac1a7116
MisterMartin <mistermartin@users.noreply.github.com>
2023-05-03 14:41:34 +1200

    Import sys

--- PyPackage/__init__.py
+++ PyPackage/__init__.py
@@ -1,4 +1,5 @@
 import os
+import sys
 # Add the directory of this package to the path, so that
 # imports can find other modules in site-packages/<package>.
 sys.path.append(os.path.dirname(__file__))
