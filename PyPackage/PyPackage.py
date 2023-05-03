2cf27ec71142defd69366ab64428996282394489
Charlie Martin <MisterMartin@users.noreply.github.com>
2023-05-03 14:32:29 +1200

    Update PyPackage.py

--- PyPackage/PyPackage.py
+++ PyPackage/PyPackage.py
@@ -1,9 +1,9 @@
-import AnotherModule as an
+import AnotherModule as a
 
 class PyPackage:
     def __init__(self):
         print(f'Welcome to {__file__}')
-        print(f'We are also accessing {an.AnotherModule().myName()}')
+        print(f'We are also accessing {a.AnotherModule().myName()}')
 
 def main():
     pp = PyPackage()
